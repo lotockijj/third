8e550cac918c1dfbd280a3c113dc3bb0585475e5
Roman Lotockiy <roman.lotockiy@diymaxeda.com>
2025-04-28 09:29:05 +0300

    Codility: ChocolatesByNumbers - not successful .

---
+++ src/main/java/org/example/codility/euclidean/algorithm/ChocolatesByNumbers.java
@@ -0,0 +1,56 @@
+package org.example.codility.euclidean.algorithm;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ChocolatesByNumbers {
+
+    //1,2,3,4,5,6,7,8,9,10,
+    public int solution(int N, int M) {
+        int result = 0;
+        int remainder0fDivision = N % M;
+        if(remainder0fDivision == 0){
+            return N/M;
+        } else {
+            int partResult = partSolution(remainder0fDivision + M, M);
+            result = N/M + partResult;
+        }
+        return result;
+    }
+
+    private int partSolution(int N, int M) {
+        Set<Integer> alreadyEaten = new HashSet<>();
+        int current = 0;
+        for (int i = 1; i <= N; i++) {
+            current++;
+            if(current == M){
+                if(alreadyEaten.contains(i)) break;
+                alreadyEaten.add(i);
+                current = 0;
+            }
+            if(i == N){
+                i = 0;
+            }
+        }
+        return N - alreadyEaten.size();
+    }
+}
+
+/*  0% :)
+public int solution(int N, int M) {
+        Set<Integer> alreadyEaten = new HashSet<>();
+        int current = 0;
+        for (int i = 1; i <= N; i++) {
+            current++;
+            if(current == M){
+                if(alreadyEaten.contains(i)) break;
+                alreadyEaten.add(i);
+                current = 0;
+            }
+            if(i == N){
+                i = 0;
+            }
+        }
+        return N - alreadyEaten.size();
+    }
+ */
---
+++ src/test/java/org/example/codility/euclidean/algorithm/ChocolatesByNumbersTest.java
@@ -0,0 +1,59 @@
+package org.example.codility.euclidean.algorithm;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChocolatesByNumbersTest {
+
+    private ChocolatesByNumbers chocolatesByNumbers;
+
+    @BeforeEach
+    void setUp() {
+        chocolatesByNumbers = new ChocolatesByNumbers();
+    }
+
+    @Test
+    void solution1() {
+        int result = chocolatesByNumbers.solution(10, 4);
+
+        assertEquals(5, result);
+    }
+
+    @Test
+    void solution2() {
+        int result = chocolatesByNumbers.solution(12, 4);
+
+        assertEquals(3, result);
+    }
+
+    @Test
+    void solution3() {
+        int result = chocolatesByNumbers.solution(17, 68);
+
+        assertEquals(3, result);
+    }
+
+    @Test
+    void solution4() {
+        int result = chocolatesByNumbers.solution(24, 18);
+
+        assertEquals(4, result);
+    }
+}
+
+/*
+Two positive integers N and M are given. Integer N represents the number of chocolates arranged in a circle, numbered from 0 to N − 1.
+You start to eat the chocolates. After eating a chocolate you leave only a wrapper.
+You begin with eating chocolate number 0. Then you omit the next M − 1 chocolates or wrappers on the circle, and eat the following one.
+More precisely, if you ate chocolate number X, then you will next eat the chocolate with number (X + M) modulo N (remainder of division).
+You stop eating when you encounter an empty wrapper.
+For example, given integers N = 10 and M = 4. You will eat the following chocolates: 0, 4, 8, 2, 6.
+The goal is to count the number of chocolates that you will eat, following the above rules.
+Write a function: class Solution { public int solution(int N, int M); }
+that, given two positive integers N and M, returns the number of chocolates that you will eat.
+For example, given integers N = 10 and M = 4. the function should return 5, as explained above.
+Write an efficient algorithm for the following assumptions:
+N and M are integers within the range [1..1,000,000,000].
+ */
