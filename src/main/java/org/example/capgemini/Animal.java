2f71588941adf3f522e58cf3fa78e8b50dc41009
Roman Lotockiy <roman.lotockiy@diymaxeda.com>
2025-05-06 16:49:04 +0300

    Codility: MaxNonoverlappingSegments 40% .

---
+++ src/main/java/org/example/capgemini/Animal.java
@@ -0,0 +1,8 @@
+package org.example.capgemini;
+
+public class Animal {
+
+    public String sound(int a){
+        return String.valueOf(a);
+    }
+}
---
+++ src/main/java/org/example/capgemini/Cat.java
@@ -0,0 +1,8 @@
+package org.example.capgemini;
+
+public class Cat extends Animal{
+
+//    private String sound(int a){
+//        return String.valueOf(a*a);
+//    }
+}
