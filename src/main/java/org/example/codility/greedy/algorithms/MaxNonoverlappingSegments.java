0a87f5a85c62f32d017a6d9842530eaab43e18a6
Roman Lotockiy <roman.lotockiy@diymaxeda.com>
2025-05-07 09:40:53 +0300

    Codility: MaxNonoverlappingSegments 100% .

--- src/main/java/org/example/codility/greedy/algorithms/MaxNonoverlappingSegments.java
+++ src/main/java/org/example/codility/greedy/algorithms/MaxNonoverlappingSegments.java
@@ -5,24 +5,26 @@ public class MaxNonoverlappingSegments {
     // 0  1  2   3 4
     //   ___     _
     //_____  __  __
-    // todo 40% (40%/40% correctness/performance !)
+    // 100% CHEERS
     public int solution(int[] A, int[] B){
         // Implement your solution here
         /*
         A = 1,3,7,9,9
         B = 5,6,8,9,10
          */
-        int result = 0;
+        if(A.length == 0) return 0;
+        int result = A.length;
         int aIndex = A[0];
         int bIndex = B[0];
         for (int i = 1; i < A.length; i++) {
             if(areTwoSegmentsOverlapped(aIndex, bIndex, A[i], B[i])){
-                result++;
+                result--;
+            } else {
+                aIndex = A[i];
+                bIndex = B[i];
             }
-            aIndex = A[i];
-            bIndex = B[i];
         }
-        return A.length - result;
+        return result;
     }
 
     private boolean areTwoSegmentsOverlapped(int aIndex, int bIndex, int a, int b) {
