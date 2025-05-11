3232b12bac5716230f76596e87198cb36b880ad5
Roman Lotockiy <roman.lotockiy@diymaxeda.com>
2025-05-11 14:57:36 +0300

    Codility: ArrayInversionCount 63% (100%/20% correctness/performance).

---
+++ src/main/java/org/example/codilitty/algorithmic/skills/ArrayInversionCount.java
@@ -0,0 +1,19 @@
+package org.example.codilitty.algorithmic.skills;
+
+public class ArrayInversionCount {
+
+    //todo 63%(100%/20% correctness/performance
+    public int solution(int[] A) {
+        // Implement your solution here
+        int result = 0;
+        //-1,6,3,4,7,4
+        for (int i = 0; i < A.length; i++) {
+            for (int j = i + 1; j < A.length; j++) {
+                if ((i > j && A[i] < A[j]) || i < j && A[i] > A[j]) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+}
