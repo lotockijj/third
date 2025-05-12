a507e9217c46f655696201b4c5b868f87dbe818a
Roman Lotockiy <roman.lotockiy@diymaxeda.com>
2025-05-12 10:08:59 +0300

    Codility: ArrayInversionCount, performance could be improved .

--- src/main/java/org/example/codilitty/algorithmic/skills/ArrayInversionCount.java
+++ src/main/java/org/example/codilitty/algorithmic/skills/ArrayInversionCount.java
@@ -3,15 +3,20 @@ package org.example.codilitty.algorithmic.skills;
 public class ArrayInversionCount {
 
     //todo 63%(100%/20% correctness/performance
+    //todo 63%(100%/20% correctness/performance
     public int solution(int[] A) {
         // Implement your solution here
         int result = 0;
         //-1,6,3,4,7,4
         for (int i = 0; i < A.length; i++) {
             for (int j = i + 1; j < A.length; j++) {
-                if ((i > j && A[i] < A[j]) || i < j && A[i] > A[j]) {
+                if (A[i] > A[j]) {
                     result++;
                 }
+            }
+            if(result > 1_000_000_000) {
+                result = -1;
+                break;
             }
         }
         return result;
