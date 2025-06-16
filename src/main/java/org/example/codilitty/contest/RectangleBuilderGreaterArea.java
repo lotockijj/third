c849f93febdbc798e28a00fa6f1ffc7d0743976a
Roman Lotockiy <roman.lotockiy@diymaxeda.com>
2025-06-16 09:46:53 +0300

    Current.

--- src/main/java/org/example/codilitty/contest/RectangleBuilderGreaterArea.java
+++ src/main/java/org/example/codilitty/contest/RectangleBuilderGreaterArea.java
@@ -1,46 +1,72 @@
 package org.example.codilitty.contest;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class RectangleBuilderGreaterArea {
 
-    //53% (76%/11% correctness/performance)
+    //todo 53% (76%/11% correctness/performance)
     public int solution(int[] A, int X) {
         // Implement your solution here
         int result = 0;
         int countOfSquares = 0;
-        Map<Integer, Integer> pens = new HashMap<>();
+        TreeMap<Integer, Integer> pens = new TreeMap<>();
         for (int i = 0; i < A.length; i++) {
             int pen = A[i];
             if (pens.containsKey(pen)) {
-                pens.put(pen, pens.get(pen) + 1);
+                int countOfPens = pens.get(pen) + 1;
+                pens.put(pen, countOfPens);
+                if (countOfPens == 4 && pen * pen >= X) {
+                    countOfSquares++;
+                }
             } else {
                 pens.put(pen, 1);
             }
         }
-        for (Map.Entry<Integer, Integer> pen : pens.entrySet()) {
-            Integer key = pen.getKey();
-            Integer value = pen.getValue();
-            if (value >= 4 && key * key >= X) {
-                countOfSquares++;
-            }
-            if(pens.get(key) > 1) {
-                result += getResult(pens, key, X);
-            }
-            if (result > 1_000_000_000) {
-                return -1;
+        result = countResult(pens, X);
+        if (result > 1_000_000_000) {
+            return -1;
+        }
+        return result + countOfSquares;
+    }
+
+    private int countResult(TreeMap<Integer, Integer> pens, int X) {
+        List<Integer> filteredPens = pens.entrySet()
+                .stream()
+                .filter(e -> e.getValue() > 1)
+                .map(Map.Entry::getKey)
+                .toList();
+        //1,3,5,7    10
+        int len = filteredPens.size() - 1;
+        for (int i = 0; i < filteredPens.size(); i++) {
+            int first = filteredPens.get(i);
+            int last = filteredPens.get(len);
+            if(first == last) break;
+            if(first * last >= X){
+               len--;
             }
         }
-        return result/2 + countOfSquares;
+        return 0;
+    }
+
+    private int fibonacci(int number) {
+        int result = 0;
+        for (int i = 2; i < number; i++) {
+            result += i;
+        }
+        return result;
     }
 
     private int getResult(Map<Integer, Integer> pens, int key, int X) {
-        int result = 0;
+        int result = 0; //1,2,5,1,1,2,3,5,1   # 5 vs 7
+        int countDown = pens.size() - 1;
         for (Map.Entry<Integer, Integer> pen : pens.entrySet()) {
-            if (pen.getKey() != key && pen.getValue() >= 2) {
-                if (key * pen.getKey() >= X) {
-                    result++;
-                }
+            if (pen.getKey() != key && pen.getKey() >= 2 && key * pen.getValue() >= X) {
+                result = countDown;
+                break;
+            } else {
+                countDown--;
             }
         }
         return result;
--- src/test/java/org/example/warmup/ConsecutiveNumbersTest.java
+++ src/test/java/org/example/warmup/ConsecutiveNumbersTest.java
@@ -1,4 +1,29 @@
+package org.example.warmup;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ConsecutiveNumbersTest {
-  
+
+    @Test
+    void solution1() {
+        int[] arr = {1, 5};
+
+        assertEquals(3, ConsecutiveNumbers.solution(arr));
+    }
+
+    @Test
+    void solution2() {
+        int[] arr = {1, 3, 5};
+
+        assertEquals(2, ConsecutiveNumbers.solution(arr));
+    }
+
+    @Test
+    void solution3() {
+        int[] arr = { 3, 4, 5, 6, 7, 8, 10};
+
+        assertEquals(1, ConsecutiveNumbers.solution(arr));
+    }
 }
