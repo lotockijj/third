6eee22373178c8fb8cf16d2ce28fc47fb31ac228
Roman Lotockiy <roman.lotockiy@diymaxeda.com>
2025-06-19 12:41:16 +0300

    Leetcode: refactoring/junits.

--- src/main/java/leetcode/one/Solution4.java
+++ src/main/java/leetcode/one/Solution4.java
@@ -1,47 +1,42 @@
 package leetcode.one;
 
 public class Solution4 {
-    public static void main(String[] args) {
-        function(new int[]{1,3}, new int[]{2});
-        function(new int[]{1,2}, new int[]{3,4});
-        function(new int[]{0, 0}, new int[]{0, 0});
-        function(new int[]{}, new int[]{1});
-    }
 
-    private static void function(int[] num1, int[] num2) {
-        int[] addArrays = new int[num1.length + num2.length];
+    public static double function(int[] nums1, int[] nums2) {
+        int[] addArrays = new int[nums1.length + nums2.length];
         int indexOfNum1 = 0;
         int indexOfNum2 = 0;
         for (int i = 0; i < addArrays.length; i++) {
-            if(indexOfNum1 < num1.length && indexOfNum2 < num2.length){
-                if(num1[indexOfNum1] > num2[indexOfNum2]){
-                    addArrays[i] = num2[indexOfNum2];
+            if (indexOfNum1 < nums1.length && indexOfNum2 < nums2.length) {
+                if (nums1[indexOfNum1] > nums2[indexOfNum2]) {
+                    addArrays[i] = nums2[indexOfNum2];
                     indexOfNum2++;
                 } else {
-                    addArrays[i] = num1[indexOfNum1];
+                    addArrays[i] = nums1[indexOfNum1];
                     indexOfNum1++;
                 }
-            } else if(indexOfNum1 < num1.length){
-                addArrays[i] = num1[indexOfNum1];
+            } else if (indexOfNum1 < nums1.length) {
+                addArrays[i] = nums1[indexOfNum1];
                 indexOfNum1++;
-            } else if(indexOfNum2 < num2.length){
-                addArrays[i] = num2[indexOfNum2];
+            } else if (indexOfNum2 < nums2.length) {
+                addArrays[i] = nums2[indexOfNum2];
                 indexOfNum2++;
             }
         }
-        double result = 0;
-        int mid = addArrays.length/2;
-        if(addArrays.length == 0){
+        double result;
+        int mid = addArrays.length / 2;
+        if (addArrays.length == 0) {
             result = 0.0;
-        } else if (addArrays.length == 1){
+        } else if (addArrays.length == 1) {
             result = addArrays[0];
-        } else if(mid %2 == 0){
-            result = ((double) addArrays[mid - 1] + addArrays[mid])/2;
+        } else if (addArrays.length % 2 == 0) {
+            result = ((double) addArrays[mid - 1] + addArrays[mid]) / 2;
         } else {
             result = addArrays[mid];
         }
-        System.out.printf("%.4f",result);
-        System.out.println();
+        return result;
+//        System.out.printf("%.4f",result);
+//        System.out.println();
     }
 }
 
