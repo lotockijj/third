bc4656ce019661db2bb4b9c1f462279a4a03b156
Roman Lotockiy <roman.lotockiy@diymaxeda.com>
2025-06-26 09:04:43 +0300

    Leetcode: 3Sums(ThreeSums15) - Time Limit Exceeded. TwoSum .

--- src/main/java/leetcode/one/ThreeSum15.java
+++ src/main/java/leetcode/one/ThreeSum15.java
@@ -1,4 +1,45 @@
 package leetcode.one;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ThreeSum15 {
+
+    //Time Limit Exceeded
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> threeSums = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (
+                            i != j &&
+                            i != k &&
+                            j != k &&
+                            nums[i] + nums[j] + nums[k] == 0
+                    ) {
+                        if (isNonPresentYet(threeSums, nums[i], nums[j], nums[k])) {
+                            threeSums.add(List.of(nums[i], nums[j], nums[k]));
+                        }
+                    }
+                }
+            }
+        }
+        return threeSums;
+    }
+
+    private boolean isNonPresentYet(List<List<Integer>> threeSums, int num, int num1, int num2) {
+        for (int i = 0; i < threeSums.size(); i++) {
+            List<Integer> integers = threeSums.get(i);
+            if (integers.get(0) == (num) &&
+                    integers.get(1) == num1 &&
+                    integers.get(2) == num2) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
