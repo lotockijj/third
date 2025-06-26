d3ac7904368cd1b21006a3d5314d763ff6f641da
Roman Lotockiy <roman.lotockiy@diymaxeda.com>
2025-06-26 09:33:10 +0300

    Leetcode: ThreeSum15 - Accepted .

--- src/main/java/leetcode/one/ThreeSum15.java
+++ src/main/java/leetcode/one/ThreeSum15.java
@@ -1,45 +1,31 @@
 package leetcode.one;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Set;
 import java.util.List;
+import java.util.HashSet;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class ThreeSum15 {
 
-    //Time Limit Exceeded
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> threeSums = new ArrayList<>();
+        Set<List<Integer>> threeSums = new HashSet<>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    if (
-                            i != j &&
-                            i != k &&
-                            j != k &&
-                            nums[i] + nums[j] + nums[k] == 0
-                    ) {
-                        if (isNonPresentYet(threeSums, nums[i], nums[j], nums[k])) {
-                            threeSums.add(List.of(nums[i], nums[j], nums[k]));
-                        }
-                    }
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                if (nums[i] + nums[j] + nums[k] > 0) {
+                    k--;
+                } else if (nums[i] + nums[j] + nums[k] < 0) {
+                    j++;
+                } else {
+                    threeSums.add(List.of(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
                 }
             }
         }
-        return threeSums;
+        return new ArrayList<>(threeSums);
     }
-
-    private boolean isNonPresentYet(List<List<Integer>> threeSums, int num, int num1, int num2) {
-        for (int i = 0; i < threeSums.size(); i++) {
-            List<Integer> integers = threeSums.get(i);
-            if (integers.get(0) == (num) &&
-                    integers.get(1) == num1 &&
-                    integers.get(2) == num2) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-
 }
