28b59dffbbdd3daae507ccb85d8de6e68c4a92c4
Roman Lotockiy <roman.lotockiy@diymaxeda.com>
2025-07-02 15:58:01 +0300

    Leetcode: RemoveNthFromEnd .

--- src/main/java/leetcode/asked/ListNode.java
+++ src/main/java/leetcode/asked/ListNode.java
@@ -1,8 +1,8 @@
 package leetcode.asked;
 
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
     ListNode() {
     }
@@ -14,6 +14,18 @@ public class ListNode {
     ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
+    }
+
+    public static ListNode getListOfNodes(int... i) {
+        if(i.length == 0) return new ListNode();
+        ListNode listNode = new ListNode(i[0]);
+        for (int j = 1; j < i.length; j++) {
+            ListNode listNext = new ListNode();
+            listNext.val = i[j];
+            listNext.next = listNode;
+            listNode = listNext;
+        }
+        return listNode;
     }
 }
 
