79218b18696de941888c3f15958bab962edc5262
Roman Lotockiy <roman.lotockiy@diymaxeda.com>
2025-08-12 15:18:09 +0300

    Codility: TwoNumbersSum - changed to list of integers, improved a bit of runtime/memory.

--- src/main/java/leetcode/problems/TwoNumbersSum.java
+++ src/main/java/leetcode/problems/TwoNumbersSum.java
@@ -1,11 +1,12 @@
 package leetcode.problems;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TwoNumbersSum {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        StringBuilder res = new StringBuilder();
+        List<Integer> res = new ArrayList<>();
         int tenth = 0;
         while (!(l1 == null && l2 == null)) {
             if (l1 != null && l2 != null) {
@@ -16,7 +17,7 @@ public class TwoNumbersSum {
                 } else {
                     tenth = 0;
                 }
-                res.append(sum);
+                res.add(sum);
                 l1 = l1.next;
                 l2 = l2.next;
             } else if (l1 != null) {
@@ -27,7 +28,7 @@ public class TwoNumbersSum {
                 } else {
                     tenth = 0;
                 }
-                res.append(sum);
+                res.add(sum);
                 l1 = l1.next;
             } else {
                 int sum = l2.val + tenth;
@@ -37,22 +38,20 @@ public class TwoNumbersSum {
                 } else {
                     tenth = 0;
                 }
-                res.append(sum);
+                res.add(sum);
                 l2 = l2.next;
             }
         }
         if (tenth == 1) {
-            res.append(tenth);
+            res.add(tenth);
         }
-        int[] numbers = Arrays.stream(res.toString().split(""))
-                .mapToInt(Integer::parseInt).toArray();
-        return createListNode(numbers);
+        return createListNode(res);
     }
 
-    private ListNode createListNode(int... arr) {
-        ListNode listNode = new ListNode(arr[arr.length - 1], null);
-        for (int i = arr.length - 2; i >= 0; i--) {
-            listNode = new ListNode(arr[i], listNode);
+    private ListNode createListNode(List<Integer> res) {
+        ListNode listNode = new ListNode(res.get(res.size() - 1), null);
+        for (int i = res.size() - 2; i >= 0; i--) {
+            listNode = new ListNode(res.get(i), listNode);
         }
         return listNode;
     }
