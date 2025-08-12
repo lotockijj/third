6e2da257f114636ef1250f12b7e93065861d483c
Roman Lotockiy <roman.lotockiy@diymaxeda.com>
2025-08-12 15:46:16 +0300

    Codility: TwoNumbersSum - changed to list of integers, improved a bit of runtime/memory.

--- src/main/java/leetcode/problems/TwoNumbersSum.java
+++ src/main/java/leetcode/problems/TwoNumbersSum.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 public class TwoNumbersSum {
 
+    //Runtime 11.61 % , Memory 8.16%
+    //Runtime 11.61 %, Memory 83.76%
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        List<Integer> res = new ArrayList<>();
+        List<ListNode> res = new ArrayList<>();
         int tenth = 0;
         while (!(l1 == null && l2 == null)) {
             if (l1 != null && l2 != null) {
@@ -17,7 +19,7 @@ public class TwoNumbersSum {
                 } else {
                     tenth = 0;
                 }
-                res.add(sum);
+                res.add(new ListNode(sum, null));
                 l1 = l1.next;
                 l2 = l2.next;
             } else if (l1 != null) {
@@ -28,7 +30,7 @@ public class TwoNumbersSum {
                 } else {
                     tenth = 0;
                 }
-                res.add(sum);
+                res.add(new ListNode(sum, null));
                 l1 = l1.next;
             } else {
                 int sum = l2.val + tenth;
@@ -38,22 +40,25 @@ public class TwoNumbersSum {
                 } else {
                     tenth = 0;
                 }
-                res.add(sum);
+                res.add(new ListNode(sum, null));
                 l2 = l2.next;
             }
         }
         if (tenth == 1) {
-            res.add(tenth);
+            res.add(new ListNode(tenth, null));
         }
         return createListNode(res);
     }
 
-    private ListNode createListNode(List<Integer> res) {
-        ListNode listNode = new ListNode(res.get(res.size() - 1), null);
+    private ListNode createListNode(List<ListNode> res) {
+        ListNode listNode = res.get(res.size() - 1);
+        ListNode tempNode = listNode;
         for (int i = res.size() - 2; i >= 0; i--) {
-            listNode = new ListNode(res.get(i), listNode);
+            tempNode = res.get(i);
+            tempNode.next = listNode;
+            listNode = tempNode;
         }
-        return listNode;
+        return tempNode;
     }
 
 }
